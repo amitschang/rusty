f4df37452ed37c0c2d0c0d5622c06873a4c0e413
Arik Mitschang <arik@jhu.edu>
2024-05-24 10:49:52 -0400

    Update tree with split stop condition handling

--- src/tree.rs
+++ src/tree.rs
@@ -1,7 +1,9 @@
 use std::{
-    sync::{mpsc, Arc, Mutex},
-    thread,
+    sync::{atomic::AtomicI32, mpsc, Arc, Mutex},
+    thread, usize,
 };
+
+const NTHREADS: u32 = 4;
 
 /// given an array of index positions that represent a sort order, reorder given
 /// array by those indexes. Buffer is a slice of the same len as input that represents
@@ -44,6 +46,29 @@ fn multi_sort_and_split(mut df: Vec<&mut [i32]>, by: usize) -> (Vec<&mut [i32]>,
     (out_a, out_b)
 }
 
+/// Work management tracking structure. This is passed between threads to request
+/// a split be worked on and indicates the dimension on which the split was originally
+/// created (or usize MAX if this is the first)
+#[derive(Debug)]
+struct WorkUnit<'a> {
+    df: Vec<&'a mut [i32]>,
+    split_dim: usize,
+}
+
+impl WorkUnit<'_> {
+    // A dummy round robin dimension selection
+    fn new_split_dim(&self) -> usize {
+        let dim = match self.split_dim {
+            usize::MAX => 0,
+            _ => self.split_dim + 1,
+        };
+        match dim >= self.df.len() {
+            true => 0,
+            false => dim,
+        }
+    }
+}
+
 /// work scheduling for iterative splits of input dataframe. Sends vectors of
 /// mutable slices (representing split nodes of dataframe) through channels to
 /// thread pool. Right now does this a fixed number of times for demo, presumably
@@ -57,21 +82,42 @@ pub fn par_mut_sorter_multi_arr(arr: &mut Vec<Vec<i32>>) {
     let (send, recv) = mpsc::channel();
     let recv = Arc::new(Mutex::new(recv));
     let send = Arc::new(send);
+    let work_counter = Arc::new(AtomicI32::new(1));  // needs to be able to go negative
     thread::scope(|scope| {
-        for _i in 0..=1 {
+        for _i in 0..NTHREADS {
             let rc = Arc::clone(&recv);
             let sc = Arc::clone(&send);
+            let wc = Arc::clone(&work_counter);
             scope.spawn(move || {
-                let r = rc.lock().unwrap().recv().unwrap();
-                let (a, b) = multi_sort_and_split(r, _i as usize);
-                sc.send(a).unwrap();
-                sc.send(b).unwrap();
+                loop {
+                    let r: WorkUnit = { rc.lock().unwrap().recv().unwrap() };
+                    // stoping condition, here we just fake it with a condition
+                    // of a min leaf size of 2.
+                    if r.df[0].len() < 3 {
+                        // only terminate if there is no pending work left, i.e.
+                        // the value before sub is 1 or less
+                        let pending = wc.fetch_sub(1, std::sync::atomic::Ordering::SeqCst);
+                        if pending <= 1 {
+                            // signal is vector of single empty slice, passing above
+                            // condition - probably a better mechanism exists.
+                            let wu = WorkUnit { df: vec!(&mut [] as &mut[i32]), split_dim: 0 };
+                            sc.send(wu).unwrap();
+                            break;
+                        }
+                        continue;
+                    }
+                    wc.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+                    let split_dim = r.new_split_dim();
+                    let (a, b) = multi_sort_and_split(r.df, split_dim);
+                    sc.send(WorkUnit { df: a, split_dim}).unwrap();
+                    sc.send(WorkUnit { df: b, split_dim}).unwrap();
+            }
             });
         }
-        let mut init = Vec::new();
-        for i in arr.iter_mut() {
-            init.push(&mut i[..]);
-        }
+        let init = WorkUnit {
+            df: arr.iter_mut().map(|x| x.as_mut_slice()).collect::<Vec<_>>(),
+            split_dim: usize::MAX,
+        };
         send.send(init).unwrap();
     });
     let recv_u = recv.lock().unwrap();
@@ -112,10 +158,15 @@ mod test {
     #[test]
     fn test_multi() {
         let mut df = vec![
-            vec![1,2,3,4,4,3,2,1],
-            vec![5,4,3,2,2,3,4,5],
-            vec![9,3,2,8,5,7,1,0]
+            vec![1,2,3,4,4,3,2,1,99],
+            vec![5,4,3,2,2,3,4,5,-1],
+            vec![9,3,2,8,5,7,1,0,99]
         ];
         par_mut_sorter_multi_arr(&mut df);
+        assert_eq!(df, vec![
+            vec![2, 2, 1, 1, 99, 4, 3, 4, 3],
+            vec![4, 4, 5, 5, -1, 2, 3, 2, 3],
+            vec![3, 1, 9, 0, 99, 8, 2, 5, 7]]);
+        println!("at end: {:?}", df);
     }
 }
